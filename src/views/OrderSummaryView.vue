4316659fdd3fd581d7edf8acc68feccb2d5b1405
Isaac Figueiredo <pedroisaac6@hotmail.com>
2023-09-14 00:43:39 -0300

    up

--- src/views/OrderSummaryView.vue
+++ src/views/OrderSummaryView.vue
@@ -2,17 +2,25 @@
   <Navbar>
     <h1 class="text-black text-4xl font-bold mt-20 mb-8">Resumo do pedido</h1>
 
-    <div class="mt-24 mx-auto w-full px-4">
-      <div class="receipt">
-        <div class="left flex flex-col">
+    <div class="mt-12 mx-auto w-full px-4">
+      <div class="receipt w-8/12 p-12 mx-auto flex flex-col gap-y-12">
+        <div class="bottom-row flex flex-row gap-x-12 w-full">
           <div>
-            <h2>Vendedor</h2>
+            <h2 class="text-xl font-semibold mb-2">Vendedor</h2>
             <p>Nome do vendedor</p>
             <p>Email do vendedor</p>
           </div>
 
           <div>
-            <h2>Imóvel</h2>
+            <h2 class="text-xl font-semibold mb-2">Cliente</h2>
+            <p>Nome do cliente</p>
+            <p>Email do cliente</p>
+          </div>
+        </div>
+
+        <div class="bottom-row flex flex-row gap-x-12 w-full">
+          <div>
+            <h2 class="text-xl font-semibold mb-2">Imóvel</h2>
             <p>ID: <span>#1233123</span></p>
             <p>Rua Praia Bella 123, Natal RN</p>
             <p>Valor de Venda: R$ <span>13.290.123</span></p>
@@ -20,24 +28,28 @@
           </div>
 
           <div>
-            <h2>Forma de pagamento</h2>
+            <img
+              class="rounded-lg w-full object-cover"
+              src="../assets/placeholder-resumo.png"
+              alt=""
+              srcset=""
+            />
+          </div>
+        </div>
+
+        <div class="bottom-row flex flex-row gap-x-12 w-full">
+          <div>
+            <h2 class="text-xl font-semibold mb-2">Forma de pagamento</h2>
             <p>À prazo</p>
             <p><span>180</span>x de R$ <span>3.123</span></p>
           </div>
-        </div>
-        <div class="right flex flex-col">
-          <div>
-            <h2>Cliente</h2>
-            <p>Nome do cliente</p>
-            <p>Email do cliente</p>
-          </div>
 
           <div>
-            <img src="../assets/placeholder-resumo.png" alt="" srcset="" />
-          </div>
-
-          <div>
-            <a href="" class="btn">botao</a>
+            <button
+              class="btn w-full bg-greenish rounded-lg text-white font-medium hover:bg-green-900 hover:scale-105"
+            >
+              Finalizar Venda
+            </button>
           </div>
         </div>
       </div>
@@ -55,8 +67,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .receipt {
-  border: 2px solid #f1f1f1;
+  border: 2px solid #d9d9d9;
+  border-radius: 30px;
+}
+
+.bottom-row {
+  div {
+    width: 50%;
+  }
 }
 </style>
