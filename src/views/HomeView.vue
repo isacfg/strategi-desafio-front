95aa5619a5685d82f6b26a1b99c9fa58c4b8fa75
Isaac Figueiredo <pedroisaac6@hotmail.com>
2023-09-14 00:56:07 -0300

    up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,10 +13,37 @@
     </div>
 
     <!-- filter and serach -->
-    <div></div>
+    <div class="flex items-center justify-between mt-12">
+      <div class="form-control">
+        <div class="input-group">
+          <input type="text" placeholder="Search…" class="input input-bordered" />
+          <button class="btn btn-square">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <select class="select select-bordered w-full max-w-xs">
+        <option disabled selected>Who shot first?</option>
+        <option>Han Solo</option>
+        <option>Greedo</option>
+      </select>
+    </div>
 
     <!-- array cards -->
-    <div class="mt-24 custom-grid mx-auto w-full gap-12">
+    <div class="mt-12 custom-grid mx-auto w-full gap-12">
       <ImovelCard v-for="index in 9" :key="index" />
     </div>
   </Navbar>
