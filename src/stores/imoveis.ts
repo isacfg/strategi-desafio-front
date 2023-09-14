d611c2d8bb17da6665018bf85607d47fb8f29bd5
Isaac Figueiredo <pedroisaac6@hotmail.com>
2023-09-14 19:39:51 -0300

    strict mode temporaryly off

--- src/stores/imoveis.ts
+++ src/stores/imoveis.ts
@@ -335,12 +335,5 @@ export const useImoveisStore = defineStore('imoveis', {
       }
     ]
   }),
-  actions: {
-    // getImoveis(): Imovel[] {
-    //   return this.$state.imoveis
-    // },
-    // addImovel(imovel: Imovel): void {
-    //   this.$state.imoveis.push(imovel)
-    // }
-  }
+  actions: {}
 })
